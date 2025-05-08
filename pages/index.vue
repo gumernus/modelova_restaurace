a66e0bfda3d2f3631c64a86101701e13354fded3
gumernus <gumernusbussines@gmail.com>
2025-05-09 00:18:36 +0200

    Implement code changes to enhance functionality and improve performance

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
-
+      <RestauraceNavbar></RestauraceNavbar>
+      <section id="onas">
+        <RestauraceHero></RestauraceHero>
+      </section>
+      <section id="menu">
+        <RestauraceMenu></RestauraceMenu>
+      </section>
+      <section id="rezervace">
+        <RestauraceReservation></RestauraceReservation>
+      </section>
+      <section id="recenze">
+        <RestauraceReviews></RestauraceReviews>
+      </section>
+      <section id="galerie">
+        <RestauraceGallery></RestauraceGallery>
+      </section>
+      <section id="kontakty">
+        <RestauraceContacts></RestauraceContacts>
+      </section>
+      <RestauraceFooter></RestauraceFooter>
     </div>
-</template>
+  </template>
+  
+  <style setup scoped>
+  </style>
